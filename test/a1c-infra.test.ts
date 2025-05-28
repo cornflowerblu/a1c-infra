6f3136ee0280e1b944ba5be2455843f401e2fd57
Roger Urich <rogerdurich@gmail.com>
2025-05-28 08:11:00 -0500

    Initial commit

---
+++ test/a1c-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as A1CInfra from '../lib/a1c-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/a1c-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new A1CInfra.A1CInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
