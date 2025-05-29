e2a2b7839b83dfe170c9bbaca169ac0e56aa254b
Roger Urich <rogerdurich@gmail.com>
2025-05-29 09:25:58 -0500

    cleanup repo

--- lib/a1c-infra-stack.d.ts
+++
@@ -1,5 +0,0 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-export declare class A1CInfraStack10 extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps);
-}
